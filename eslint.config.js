1d28d3039562715d29a5e2b722ad98c47cbfb563
Denis <denis.chertenko@mail.ru>
2026-02-14 14:45:18 +0500

    fix linters

--- eslint.config.js
+++ eslint.config.js
@@ -1,24 +1,14 @@
-import js from '@eslint/js';
-import tseslint from 'typescript-eslint';
-import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import { defineConfig } from 'eslint/config';
-import stylistic from '@stylistic/eslint-plugin';
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
   stylistic.configs.recommended,
-  ...tseslint.configs.recommended,
-  eslintPluginPrettier,
   {
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
     extends: ['js/recommended'],
-    languageOptions: { globals: globals.node }
+    languageOptions: { globals: globals.node },
   },
-  {
-    files: ['**/*.{js,ts}'],
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off'
-    }
-  }
-]);
+])
